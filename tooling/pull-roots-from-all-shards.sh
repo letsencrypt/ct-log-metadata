51f0d0a4f9695a0ded37bf229329321ff45062fe
J.C. Jones <jc@letsencrypt.org>
2022-11-30 21:47:10 -0700

    Bring SKID processing into pull-roots-from-all-shards.sh

--- tooling/pull-roots-from-all-shards.sh
+++ tooling/pull-roots-from-all-shards.sh
@@ -13,25 +13,47 @@ function get_roots() {
     done
 }
 
+function copy() {
+    SRC="${1}"
+    DST="${2}"
+
+    if [ -r "${DST}" ] ; then
+        echo "Duplicate! ${SRC} cannot copy to ${DST} as it already exists."
+    else
+        cp "${SRC}" "${DST}"
+    fi
+}
+
 function rename_roots() {
     local LOG="${1}"
     local SHARD="${2}"
     local TEMPDIR="${3}"
+
+    mkdir -p "${LOG}-${SHARD}"
+
     for CRT in $(ls "${TEMPDIR}" | grep -E '^[0-9]*.crt'); do
-        O=$(certigo dump -f PEM --json "${TEMPDIR}/${CRT}" | jq -r '.certificates[].subject.organization[0]' | tr -d '\n' | sed -e 's|/| |g' -e 's|\\||g')
-        CN=$(certigo dump -f PEM --json "${TEMPDIR}/${CRT}" | jq -r '.certificates[].subject.common_name' | tr -d '\n' | sed -e 's|/| |g' -e 's|\\||g')
+        ROOT="${TEMPDIR}/${CRT}"
+
+        O=$(certigo dump -f PEM --json "${ROOT}" | jq -r '.certificates[].subject.organization[0]' | tr -d '\n' | sed -e 's|/| |g' -e 's|\\||g')
+        CN=$(certigo dump -f PEM --json "${ROOT}" | jq -r '.certificates[].subject.common_name' | tr -d '\n' | sed -e 's|/| |g' -e 's|\\||g')
+        SKID=$(certigo dump -f PEM --json "${ROOT}" | jq -r '.certificates[].subject.key_id' | tr -d '\n' | sed -e 's|:||g' | tr '[:upper:]' '[:lower:]')
 
         # We specifically chose not to use the SHA256 of the fingerprint, or a serial, or any other numeric identifier
         # because we want to keep these human readable.
         # The literal null comes from jq
+
+        if [ "${SKID}" == "null" ]; then
+            SKID="0"
+        fi
+
         if [ "${O}" == "null" ]; then
-            cp "${TEMPDIR}/${CRT}" "${LOG}/${CN}.crt"
+            copy "${ROOT}" "${LOG}-${SHARD}/${CN} - ${SKID}.crt"
         elif [ "${CN}" == "null" ]; then
-            cp "${TEMPDIR}/${CRT}" "${LOG}/${O}.crt"
+            copy "${ROOT}" "${LOG}-${SHARD}/${O} - ${SKID}.crt"
         elif [ "${CN}" == "null" ] && [ "${O}" == "null" ]; then
-            prettyRed "${TEMPDIR}/${CRT} is borked"
+            copy "${ROOT}" "${LOG}-${SHARD}/${SKID}.crt"
         else
-            cp "${TEMPDIR}/${CRT}" "${LOG}/${O} - ${CN}.crt"
+            copy "${ROOT}" "${LOG}-${SHARD}/${O} - ${CN} - ${SKID}.crt"
         fi
     done
 }
